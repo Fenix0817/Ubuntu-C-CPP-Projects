f4c9ae1a7bf127c47c89b79035ae7cfa625ed750
Jack Armstrong <mackycheese21@gmail.com>
2018-09-28 20:29:21 -0700

    Added book credits

--- RTFTGU/main.cpp
+++ RTFTGU/main.cpp
@@ -7,6 +7,15 @@
 
 #include "utils.h"
 
+//Raytracing From The Ground Up
+//ISBN: 978-1-56881-272-4
+//By Kevin Suffern
+//
+//This project is from following the book.
+//You should get it.
+//It is an awesome book!
+//Explains everything really well.
+
 int main(){
 	//TODO: Camera class, plane class, tracer class.....
 }
