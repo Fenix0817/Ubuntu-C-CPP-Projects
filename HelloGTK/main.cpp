558ded53d8a4164ccd874c7750f64a0f9c2d39dc
Jack Armstrong <mackycheese21@gmail.com>
2018-11-13 20:31:54 -0800

    Removed unused code in HelloGTK

--- HelloGTK/main.cpp
+++ HelloGTK/main.cpp
@@ -11,29 +11,13 @@ int main(int argc, char *argv[]) {
 
 	gtk::init(argc,argv);
 
+	//TODO: Fully implement GTK object heirarchy, with boxes and bins and stuff
+	//TODO: This should be a full translation, not just a select library
 	gtk::Window*window=new gtk::Window();
 	window->create(GTK_WINDOW_TOPLEVEL);
 	window->setPosition(GTK_WIN_POS_CENTER);
 	window->setSize(500,500);
 	window->setTitle("Hello GTK");
-
-	gtk::Box*box=new gtk::Box();
-	box->create(GTK_ORIENTATION_VERTICAL,100);
-
-	gtk::Label*label=new gtk::Label();
-	label->create();
-	label->setText("Label");
-	box->add(label);
-
-	gtk::ButtonBox*btnBox=new gtk::ButtonBox();
-	btnBox->create(GTK_ORIENTATION_VERTICAL);
-	gtk::Button*btn=new gtk::Button();
-	btn->create();
-	btn->addLabel("Button");
-	btnBox->add(btn);
-	box->add(btnBox);
-
-//	window->add(box);
 
 	gtk::xml::UI*ui=new gtk::xml::UI();
 	ui->loadUI("ui.xml","my_ui");
